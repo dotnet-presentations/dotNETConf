1ee8691fcc0c96be1a2d10e7aabebf74a48a73cf
brady gaster <bradyg@microsoft.com>
2020-02-15 09:53:27 -0800

    removed old projects, added pwa support

---
+++ 2019/Technical/keynote/BlazorWeather.Server/wwwroot/ServiceWorker.js
@@ -0,0 +1,9 @@
+const baseURL = '/';
+const indexURL = '/Pages/_Host.cshtml';
+const networkFetchEvent = 'fetch';
+const swInstallEvent = 'install';
+const swInstalledEvent = 'installed';
+const swActivateEvent = 'activate';
+self.addEventListener(networkFetchEvent, event => {
+    return fetch(event.request);
+});
--- 2019/Technical/keynote/BlazorWeather.WebAssembly/wwwroot/service-worker.js
+++
@@ -1,8 +0,0 @@
-﻿self.addEventListener('install', async event => {
-    console.log('Installing service worker...');
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-    return null;
-});
