6b0fd9f05ef0d2b0da5f8a4de363955997ac3e70
brady gaster <bradyg@microsoft.com>
2020-07-27 18:08:01 -0700

    upping to 5

--- 2019/Technical/keynote/ClimateControl/Program.cs
+++ 2019/Technical/keynote/ClimateControl/Program.cs
@@ -1,54 +1,38 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+//using ClimateControl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace ClimateControl
+WebApplication.Create(args).MapGet("/temperature/{temp}", async http =>
 {
-    public class Program
+    //var ledService = new LEDService();
+    var perfectTemperature = int.Parse(app.Configuration["perfectTemperature"]);
+    var tempRouteValue = (string)http.Request.RouteValues["temp"];
+    var temp = 0.0;
+    var isSuccess = double.TryParse(tempRouteValue, out temp);
+    var tolerance = 3;
+
+    if (isSuccess)
     {
-        static async Task Main(string[] args)
+        if (temp <= (perfectTemperature - tolerance))
         {
-            var ledService = new LEDService();
-            var app = WebApplication.Create(args);
-
-            app.MapGet("/temperature/{temp}", async http =>
-            {
-                var perfectTemperature = int.Parse(app.Configuration["perfectTemperature"]);
-                var tempRouteValue = (string)http.Request.RouteValues["temp"];
-                var temp = 0.0;
-                var isSuccess = double.TryParse(tempRouteValue, out temp);
-                var tolerance = 3;
-
-                if (isSuccess)
-                {
-                    if(temp <= (perfectTemperature - tolerance))
-                    {
-                        Console.WriteLine("Too cold!");
-                        ledService.ToggleGold(true);
-                    }
-                    else if(temp >= (perfectTemperature + tolerance))
-                    {
-                        Console.WriteLine("Too hot!");
-                        ledService.ToggleRed(true);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Just right!");
-                        ledService.ToggleGreen(true);
-                    }
-                }
-
-                await http.Response.WriteJsonAsync(new { success = isSuccess });
-            });
-
-            await app.RunAsync();
+            Console.WriteLine("Too cold!");
+            //ledService.ToggleGold(true);
+        }
+        else if (temp >= (perfectTemperature + tolerance))
+        {
+            Console.WriteLine("Too hot!");
+            //ledService.ToggleRed(true);
+        }
+        else
+        {
+            Console.WriteLine("Just right!");
+            //ledService.ToggleGreen(true);
         }
     }
-}
+
+    await http.Response.WriteJsonAsync(new { success = isSuccess });
+}).RunAsync();
